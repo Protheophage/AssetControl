1c83c327f228de7bd3f00bd859c99e5add64ebf3
Colby Connolly <cconnolly@aotwinds.com>
2019-04-16 02:58:00 -0700

    Created Get-NextAssetID

---
+++ Asset_Tracking/Get_NextAssetID.psm1
@@ -0,0 +1,21 @@
+Function Get-NextAssetID
+{
+	<#
+	.SYNOPSIS
+	Retrieve the next available Asset ID from SQL
+	#>
+	
+    ##Set Location of PS instance to SQL Database
+	Set-Location SQLSERVER:\SQL\PROMETHEUS\DEFAULT\Databases\Assets\Tables
+	
+	##Set variable for asset id
+	$Assetted = Invoke-Sqlcmd "SELECT MAX(asset_id)+1 FROM dbo.AssetList Where asset_id IS NOT NULL AND asset_id < 9990000;"
+	$Assetted = $Assetted.column1
+
+	##Return to default PS location
+	Set-Location C:\WINDOWS\system32
+
+    ##Display Asset ID to user
+	$wshell = New-Object -ComObject Wscript.Shell
+	$wshell.Popup("The next available Asset ID is $($Assetted).",0,"Progress Update",0x0)
+}
