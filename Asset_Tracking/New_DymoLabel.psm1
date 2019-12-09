db8e550b974dd7fb279e8040536ee6b414cec023
Colby Connolly <cconnolly@aotwinds.com>
2019-12-09 05:58:56 -0800

    "Updated for current Dymo printer share of \\IT-SYN\IT DYMO"

--- Asset_Tracking/New_DymoLabel.psm1
+++ Asset_Tracking/New_DymoLabel.psm1
@@ -30,8 +30,8 @@ Function New-DymoLabel
 		If(!$dymoPrint)
 		{
 			Write-Output 'You do not have a DYMO printer'
-			Write-Output 'Adding DYMO printer \\IT-TV\DYMO-SOC'
-			Add-Printer -ConnectionName \\IT-TV\DYMO-SOC
+			Write-Output 'Adding DYMO printer \\IT-SYN\IT DYMO'
+			Add-Printer -ConnectionName "\\IT-SYN\IT DYMO"
 		}
 		$dymmoSoft = get-package -Name "*Dymo Label*" -ErrorAction SilentlyContinue
 		If(!$dymmoSoft)
